91ed5faf4951c40632987487fda767db2d787094
bimacahyosaktiutomo <bimasakticahyoutomo@gmail.com>
2024-09-20 01:24:14 +0800

    Posttest 2

--- sistem_informasi_ayam/sia/models/produks.py
+++ sistem_informasi_ayam/sia/models/produks.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Produks(models.Model):
     id = models.AutoField(unique=True, primary_key=True)
-    name = models.CharField(max_length=255)
-    description= models.CharField(max_length=13)
+    name = models.CharField(max_length=13)
+    description= models.CharField(max_length=255)
     kategori = models.CharField(max_length=13)
     stok = models.IntegerField()
 
