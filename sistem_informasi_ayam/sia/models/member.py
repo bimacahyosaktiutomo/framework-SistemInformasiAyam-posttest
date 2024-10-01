0fd1d583cec093b99af1b8fe802c9f4a9f101979
bimacahyosaktiutomo <bimasakticahyoutomo@gmail.com>
2024-10-01 09:42:09 +0800

    Posttest 3

--- sistem_informasi_ayam/sia/models/member.py
+++ sistem_informasi_ayam/sia/models/member.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Members(models.Model):
     id = models.AutoField(unique=True, primary_key=True)
-    email = models.EmailField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=13, unique=True)
     password = models.CharField(max_length=13)
 
