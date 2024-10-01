f3f500ee26f0c4f84b1ab78e8d23cbc4fb653f1b
bimacahyosaktiutomo <bimasakticahyoutomo@gmail.com>
2024-10-01 09:41:02 +0800

    Posttest 3

--- sistem_informasi_ayam/sia/models/overseer.py
+++ sistem_informasi_ayam/sia/models/overseer.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Overseer(models.Model):
     id = models.AutoField(unique=True, primary_key=True)
-    email = models.EmailField(max_length=20)
+    email = models.EmailField(max_length=20, unique=True)
     username = models.CharField(max_length=13, unique=True)
     password = models.CharField(max_length=13)
     priviliege = models.CharField(max_length=13)
